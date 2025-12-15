42b2baae43bf42ce52daf39d54a18d1af4f19178
Andrii <andriy07da@gmail.com>
2025-12-15 14:48:05 +0200

    Add OpenSans font

--- Engine/src/Core/Resources/DefaultAssets.hpp
+++ Engine/src/Core/Resources/DefaultAssets.hpp
@@ -5,4 +5,5 @@ namespace GameEngine
 {
     constexpr std::string_view AssetsRelativePath = GAMEENGINE_ASSETS_RELATIVE_PATH;
     constexpr std::string_view DefaultShaderRegistryPath = GAMEENGINE_ASSETS_RELATIVE_PATH "/registries/shader_registry.json";
+    constexpr std::string_view OpenSansTtfFontPath = GAMEENGINE_ASSETS_RELATIVE_PATH "/fonts/OpenSans.ttf";
 }
